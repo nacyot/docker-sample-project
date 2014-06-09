c73d839426b2797bc053132f5c13023925fea941
Kim Daekwon <propellerheaven@gmail.com>
2014-06-09 15:02:57 +0900

    Add Ip address to post#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @ip = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
   end
 
   # GET /posts/1
